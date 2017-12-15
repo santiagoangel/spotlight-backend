6ad3b219a6658239e0d3cadafa31b06d7b1fcf07
Santiago Angel <santiagoangel@gmail.com>
2017-12-15 17:42:20 -0600

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Saludos, %s!";
+    private static final String template = "Hi, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="Spotlight") String name) {
+    @CrossOrigin(origins = "*")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Bank customer") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
