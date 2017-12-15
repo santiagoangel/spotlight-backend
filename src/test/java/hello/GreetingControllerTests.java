8b8e0aa2e6a0ceccf9b885b052e5449d3fe5c170
Santiago Angel <santiagoangel@gmail.com>
2017-12-15 17:43:12 -0600

    Update GreetingControllerTests.java

--- src/test/java/hello/GreetingControllerTests.java
+++ src/test/java/hello/GreetingControllerTests.java
@@ -40,7 +40,7 @@ public class GreetingControllerTests {
     public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
         this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Saludos, Spotlight!"));
+                .andExpect(jsonPath("$.content").value("Hi, Bank customer!"));
     }
 
     @Test
@@ -48,7 +48,7 @@ public class GreetingControllerTests {
 
         this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
                 .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Saludos, Spring Community!"));
+                .andExpect(jsonPath("$.content").value("Hi, Spring Community!"));
     }
 
 }
